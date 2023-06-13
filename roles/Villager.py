3cd12ec51ab576217f49d4740345a65b038cd9bb
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-13 20:33:57 +0200

    Update Villager.py

--- roles/Villager.py
+++ roles/Villager.py
@@ -1,10 +1,17 @@
-class Villager:
+from roles.agent import Agent
+
+class Villager(Agent):
     def __init__(self):
-        pass
+        super().__init__("villager")
 
     def init_kripke_model(self):
         """Builds the initial Kripke model world, where everyone can be any role"""
         pass
     
-    def vote(self):
-        pass
+    # In here we can implement how the villagers are going to be communicating
+    def discuss(self):
+        print("blalbalblaa")
+
+    def vote(self, players):
+        return(self)
+
