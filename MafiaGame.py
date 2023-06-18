c2bc8fd83d349284065b3a4820a2dc76c25b0df0
Pieter-Jacobs <pieternee@gmail.com>
2023-06-18 15:54:27 +0200

    relation initialization working

--- MafiaGame.py
+++ MafiaGame.py
@@ -1,8 +1,8 @@
 from mlsolver.kripke import World, KripkeStructure
 from itertools import permutations
-from roles.detective import Detective
-from roles.villager import Villager
-from roles.mafia import Mafia
+from roles.Detective import Detective
+from roles.Villager import Villager
+from roles.Mafia import Mafia
 from phases.day import Day
 from phases.night import Night
 
@@ -17,18 +17,40 @@ class MafiaGame:
         """Builds the initial Kripke model world, where everyone believes everyone can be any role,
         atom m1: agent 1 is mafia
         atom d1: agent 1 is detective
-        atom v1: agent 1 isvillager
+        atom v1: agent 1 is villager
         """
-        atoms = ['v' for i in range(n_villagers)] + ['m' for i in range(n_mafia)] + ['d' for i in range(n_detective)]
+        agents = ['agent' + str(i + 1) for i in range(n_villagers + n_mafia + n_detective)]
 
-        # --- TODO When creating the worlds, we can assign ID's to the agents depending on the assigned roles in the world. ---#
-        all_possible_worlds = [list(per) for per in set(permutations(atoms))]
-        for i in range(len(all_possible_worlds)):
-            for j in range(len(all_possible_worlds[i])):
-                all_possible_worlds[i][j] = str(j) + ":" + all_possible_worlds[i][j] 
-        #print(all_possible_worlds)
-        pass
+        # Create all possible combinations of agent roles
+        game_roles = ['v'] * n_villagers + ['m'] * n_mafia + ['d'] * n_detective
+        all_possible_roles = set(permutations(game_roles))
 
+        # Create the worlds for the Kripke structure
+        worlds = []
+        for roles in all_possible_roles:
+            world_name = ':'.join([agent + ':' + role for agent, role in zip(agents, roles)])
+            world = World(world_name, {})
+            worlds.append(world)
+
+        # Create the relations dictionary based on the agent's role in each world
+        relations = {}
+        for i, agent in enumerate(agents):
+            agent_relations = []
+            for j, world in enumerate(worlds):
+                agent_roles = world.name.split(':')[1::2]
+                agent_role = agent_roles[i]
+                for world_2 in worlds[j:]: 
+                    agent_roles = world_2.name.split(':')[1::2] 
+                    if agent_roles[i] == agent_role:
+                        agent_relations.append((world.name, world_2.name))
+            relations[agent] = set(agent_relations)
+            
+        # Create the Kripke structure
+        ks = KripkeStructure(worlds, relations)
+
+
+        return ks
+        
     # Creates the players without ID
     def init_players(self, n_villagers, n_mafia, n_detective):
         self.players=[]
