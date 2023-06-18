99c4dd3178e10c5d192c9261dee971997f3b45de
Pieter-Jacobs <pieternee@gmail.com>
2023-06-18 17:07:38 +0200

    Fully functioning relations

--- MafiaGame.py
+++ MafiaGame.py
@@ -5,7 +5,8 @@ from roles.Villager import Villager
 from roles.Mafia import Mafia
 from phases.day import Day
 from phases.night import Night
-
+import matplotlib.pyplot as plt
+import networkx as nx
 
 class MafiaGame:
     def __init__(self, n_villagers, n_mafia, n_detective):
@@ -39,18 +40,68 @@ class MafiaGame:
             for j, world in enumerate(worlds):
                 agent_roles = world.name.split(':')[1::2]
                 agent_role = agent_roles[i]
-                for world_2 in worlds[j:]: 
+                for world_2 in worlds: 
                     agent_roles = world_2.name.split(':')[1::2] 
                     if agent_roles[i] == agent_role:
                         agent_relations.append((world.name, world_2.name))
             relations[agent] = set(agent_relations)
-            
+
         # Create the Kripke structure
         ks = KripkeStructure(worlds, relations)
-
-
+        self.visualize_kripke_model(ks)
         return ks
-        
+    
+    def visualize_kripke_model(self, kripke_model):
+        """Visualizes the Kripke model worlds and relations."""
+        # Create an empty directed graph
+        graph = nx.DiGraph()
+
+        # Add nodes (worlds) to the graph
+        for world in kripke_model.worlds:
+            graph.add_node(world.name)
+
+        # Add edges (relations) to the graph
+        for agent, relations in kripke_model.relations.items():
+            for relation in relations:
+                # If we already had another agent with this edge, we expand the agent array
+                if relation in graph.edges:
+                    current_labels = graph.edges[relation]['labels']
+                    current_labels.append('R' + agent[5:])
+                    graph.edges[relation]['labels'] = current_labels
+                else:
+                    graph.add_edge(relation[0], relation[1], labels=['R' + agent[5:]])
+
+        # Set node positions using a spring layout algorithm
+        pos = nx.spring_layout(graph)
+
+        # Draw nodes (worlds)
+        nx.draw_networkx_nodes(graph, pos, node_color='lightblue', node_size=500)
+
+        # Draw edges (relations)
+        nx.draw_networkx_edges(graph, pos, arrowstyle='->', arrowsize=10, edge_color='gray')
+
+        # Draw labels (world roles and relation labels)
+        labels = {}
+        edge_labels = {}
+
+        for node, data in graph.nodes(data=True):
+            roles = node.split(':')[1::2]
+            role_label = ''.join(roles)
+            labels[node] = role_label
+
+        for u, v, data in graph.edges(data=True):
+            edge_labels[(u, v)] = ",".join(data['labels'])
+
+        nx.draw_networkx_labels(graph, pos, labels=labels, font_size=10, font_color='black')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_size=8, font_color='gray')
+
+        # Set plot title
+        plt.title("Kripke Model Visualization")
+
+        # Show the plot
+        plt.axis('off')
+        plt.show()
+                
     # Creates the players without ID
     def init_players(self, n_villagers, n_mafia, n_detective):
         self.players=[]
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MafiaGame import MafiaGame
 
 
 def main():
-   mafiaGame = MafiaGame(1,1,1)
+   mafiaGame = MafiaGame(2,1,0)
    mafiaGame.start()
    
 if __name__ == '__main__':
