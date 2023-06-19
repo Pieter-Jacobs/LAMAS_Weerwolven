cd2ff2c98d018d2b52e3b4b09c9a1c71f53bb1dc
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-19 19:19:51 +0200

    Update day.py

--- phases/day.py
+++ phases/day.py
@@ -1,6 +1,9 @@
 class Day:
-    def __init__(self, players):
+    def __init__(self, players, n_villagers, n_mafia, n_detectives):
         self.players = players
+        self.n_villagers = n_villagers
+        self.n_mafia = n_mafia
+        self.n_detectives = n_detectives
 
     # Public announcement is made, adding the information to each players' knowledge
     def announcement_phase(self, information):
@@ -13,7 +16,9 @@ class Day:
             #might not use this and instead just do the talking here, but its here if needed
             player.discuss()
 
+    # Allows agents to vote based on their knowledge
     def voting_phase(self):
+        print("Voting phase has started \n")
         votes = {}
         for player in self.players:
             vote = player.vote(self.players)
@@ -21,18 +26,8 @@ class Day:
                 votes[vote] += 1
             else:
                 votes[vote] = 1
+        max_votes = max(votes.values())
+        removed_player = [player for player, vote_count in votes.items() if vote_count == max_votes]
+        return removed_player
 
-    def game_status(self):
-        number_players = 0
-        number_mafia = 0
-        for player in self.players:
-            if player.role == "villager" or player.role == "detective":
-                number_players = number_players + 1
-            if player.role == "mafia":
-                number_mafia = number_mafia + 1
-        if number_mafia == 0:
-            print("Villagers won!")
-        elif number_mafia > number_players:
-            print("Mafia won!")
-        else:
-            print("Game not over yet")
+    
