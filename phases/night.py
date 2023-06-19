543a0b900b7cac8f5c2c9e3f2d1a7d697cb7fd77
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-19 19:20:00 +0200

    Update night.py

--- phases/night.py
+++ phases/night.py
@@ -1,28 +1,30 @@
+import random
+
 class Night:
-    def __init__(self, players):
+    def __init__(self, players, n_villagers, n_mafia, n_detectives):
         self.players = players
+        self.n_villagers = n_villagers
+        self.n_mafia = n_mafia
+        self.n_detectives = n_detectives
 
     def mafia_phase(self):
+        print("Mafia phase has started \n")
         for player in self.players:
-            if player.role == "Mafia":
-                pass
+            if player.role == "mafia":
+                killable_players = [p for p in self.players if p != player and p.role != "mafia"]
+                voted_player = random.choice(killable_players)
+                return voted_player
 
     def detective_phase(self):
+        print("Detective phase has started \n")
         for player in self.players:
-            if player.role == "Detective":
-                pass
+            if player.role == "detective":
+                eligible_playes = [p for p in self.players if p != player]
+                random_player = random.choice(eligible_playes)
+                if random_player.role == 'mafia':
+                    #add this knowledge
+                    print("The detective now knows the chosen player to be a mafia! \n")
+                else:
+                    #add this knowledge
+                    print("The detective now knows the chosen player to be a villager! \n")
             
-    def game_status(self):
-        number_players = 0
-        number_mafia = 0
-        for player in self.players:
-            if player.role == "villager" or player.role == "detective":
-                number_players = number_players + 1
-            if player.role == "mafia":
-                number_mafia = number_mafia + 1
-        if number_mafia == 0:
-            print("Villagers won!")
-        elif number_mafia > number_players:
-            print("Mafia won!")
-        else:
-            print("Game not over yet")       
