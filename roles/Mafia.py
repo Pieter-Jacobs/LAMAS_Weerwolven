e19e2efea742f725fa870f753b1b7507f130e003
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-20 10:14:55 +0200

    Update Mafia.py

--- roles/Mafia.py
+++ roles/Mafia.py
@@ -11,8 +11,8 @@ class Mafia(Agent):
     def discuss(self):
         print("blalbalblaa")
 
-    def vote(self, players):
-        eligible_playes = [p for p in players if p != self]
+    def vote(self, players, removed_players):
+        eligible_playes = [p for p in players if p != self and p not in removed_players]
         random_player = random.choice(eligible_playes)
         return random_player
 
