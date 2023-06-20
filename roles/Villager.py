50e91cc1130be1d23ae5d74b8cca66a7bce22ae7
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-20 10:14:45 +0200

    Update Villager.py

--- roles/Villager.py
+++ roles/Villager.py
@@ -4,17 +4,13 @@ import random
 class Villager(Agent):
     def __init__(self):
         super().__init__("villager")
-
-    def init_kripke_model(self):
-        """Builds the initial Kripke model world, where everyone can be any role"""
-        pass
     
     # In here we can implement how the villagers are going to be communicating
     def discuss(self):
         print("blalbalblaa")
 
-    def vote(self, players):
-        eligible_playes = [p for p in players if p != self]
+    def vote(self, players, removed_players):
+        eligible_playes = [p for p in players if p != self and p not in removed_players]
         random_player = random.choice(eligible_playes)
         return random_player
 
