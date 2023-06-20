a27dacee2a09eb139919e89cd03ec75843f9b81a
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-20 10:15:04 +0200

    Update Detective.py

--- roles/Detective.py
+++ roles/Detective.py
@@ -12,8 +12,8 @@ class Detective(Agent):
     def discuss(self):
         print("blalbalblaa")
 
-    def vote(self, players):
-        eligible_playes = [p for p in players if p != self]
+    def vote(self, players, removed_players):
+        eligible_playes = [p for p in players if p != self and p not in removed_players]
         random_player = random.choice(eligible_playes)
         return random_player
     
