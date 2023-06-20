1381d6e3b81371f961f37215c59e8cbce05b801e
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-20 10:15:40 +0200

    Update night.py

--- phases/night.py
+++ phases/night.py
@@ -7,19 +7,19 @@ class Night:
         self.n_mafia = n_mafia
         self.n_detectives = n_detectives
 
-    def mafia_phase(self):
-        print("Mafia phase has started \n")
+    def mafia_phase(self, removed_players):
+        print("-------------------------------- Mafia phase has started --------------------------------\n")
         for player in self.players:
             if player.role == "mafia":
-                killable_players = [p for p in self.players if p != player and p.role != "mafia"]
+                killable_players = [p for p in self.players if p != player and p.role != "mafia" and p not in removed_players]
                 voted_player = random.choice(killable_players)
                 return voted_player
 
     def detective_phase(self):
-        print("Detective phase has started \n")
+        print("-------------------------------- Detective phase has started --------------------------------\n")
         for player in self.players:
             if player.role == "detective":
-                eligible_playes = [p for p in self.players if p != player]
+                eligible_playes = [p for p in self.players if p != player and p.role != 'detective']
                 random_player = random.choice(eligible_playes)
                 if random_player.role == 'mafia':
                     #add this knowledge
@@ -27,4 +27,4 @@ class Night:
                 else:
                     #add this knowledge
                     print("The detective now knows the chosen player to be a villager! \n")
-            
+
