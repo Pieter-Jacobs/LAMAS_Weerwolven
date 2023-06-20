40b3cea78ad0d5b8eee603c415bc6d87027dab14
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-20 18:11:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from MafiaGame import MafiaGame
 
 def main():
    mafiaGame = MafiaGame(2,1,1)
-   mafiaGame.day.talking_round()
+   #mafiaGame.day.talking_round()
+   mafiaGame.start()
 
 if __name__ == '__main__':
    main() 
