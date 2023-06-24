3eb3e519561c54b685dd3f0e6aed272e5b1656c0
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-24 10:21:55 +0200

    Update Mafia.py

--- roles/Mafia.py
+++ roles/Mafia.py
@@ -5,14 +5,8 @@ class Mafia(Agent):
     def __init__(self, id):
         super().__init__("mafia", id, True)
 
-    def kill_player(self, players):
-        pass
-    
-    def discuss(self):
-        print("blalbalblaa")
-
-    def vote(self, players, removed_players):
-        eligible_playes = [p for p in players if p != self and p not in removed_players]
+    def vote(self, players):
+        eligible_playes = [p for p in players if p != self and p.alive == True]
         random_player = random.choice(eligible_playes)
         return random_player
 
