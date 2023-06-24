34da60a20deb0a6f49d0259a03e8ed09314a51fd
TwanV <47418991+TwanV@users.noreply.github.com>
2023-06-24 10:22:05 +0200

    Update Detective.py

--- roles/Detective.py
+++ roles/Detective.py
@@ -6,14 +6,8 @@ class Detective(Agent):
     def __init__(self, id):
         super().__init__("detective", id)
 
-    def inspect(self):
-        pass
-
-    def discuss(self):
-        print("blalbalblaa")
-
-    def vote(self, players, removed_players):
-        eligible_playes = [p for p in players if p != self and p not in removed_players]
+    def vote(self, players):
+        eligible_playes = [p for p in players if p != self and p.alive == True]
         random_player = random.choice(eligible_playes)
         return random_player
     
